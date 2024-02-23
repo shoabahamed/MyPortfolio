0c2e6fc5ba2349a1119cb77892ff2c3dde30e9df
shoabahamed <105428244+shoabahamed@users.noreply.github.com>
2024-02-23 15:43:25 +0600

    complete the contact me section. only some minor parts of the project is left

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+// for menubar
 const sideMenu = document.querySelector('.sidemenu');
-console.log(sideMenu);
+
 function openMenu(){
   sideMenu.style.right = "0";
 }
@@ -7,3 +8,38 @@ function openMenu(){
 function closeMenu(){
   sideMenu.style.right = "-500px";
 }
+
+// for contact form
+(function() {
+        // https://dashboard.emailjs.com/admin/account
+        emailjs.init({
+          publicKey: "OzHRXsirDSh5ptb3I",
+        });
+    })();
+
+    const msg = document.getElementById('msg');
+    window.onload = function() {
+        form = document.getElementById('contact-form');
+        document.getElementById('contact-form').addEventListener('submit', function(event) {
+            event.preventDefault();
+            emailjs.sendForm('contact_service', 'contact_form', this)
+                .then(() => {
+                    msg.innerHTML = 'Email send successfully';
+                    msg.style.visibility = 'visible';
+                    setTimeout(function(){
+                      msg.style.visibility = 'hidden';
+                    }, 5000);
+                    form.reset();
+                    console.log('SUCCESS!');
+                }, (error) => {
+                  msg.style.color = 'red';
+                  msg.innerHTML = 'Failure';
+                  msg.style.visibility = 'visible';
+                  setTimeout(function(){
+                    msg.style.visibility = 'hidden';
+                  }, 5000);
+                  msg.style.color = '#61b752';
+                  console.log('FAILED...', error);
+                });
+        });
+    }
